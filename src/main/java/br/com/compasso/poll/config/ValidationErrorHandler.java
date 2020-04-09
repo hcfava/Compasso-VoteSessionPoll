e91f7a98c5c1b31830b1a6405bef08b46608ac94
hcfava <59886550+hcfava@users.noreply.github.com>
2020-04-09 14:33:24 -0300

    Ajustados os tratamentos de Exceptions, falta testes automatizados

--- src/main/java/br/com/compasso/poll/config/ValidationErrorHandler.java
+++ src/main/java/br/com/compasso/poll/config/ValidationErrorHandler.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -40,6 +41,12 @@ public class ValidationErrorHandler {
 	@ExceptionHandler(ValidationException.class)
 	public ErrorDto handle(RuntimeException exception) {
 		return new ErrorDto(exception.getMessage());
+	}
+	
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler({HttpMessageNotReadableException.class, IllegalArgumentException.class})
+	public ErrorDto handle(Exception exception) {
+		return new ErrorDto("Valor dos campos inválidos, por favor informe apenas valores válidos");
 	}
 	
 }
